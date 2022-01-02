29c6a2ec4829e0abcc755b84af5cead6d9471a62
Anthony Vidovic <avidovic@uoguelph.ca>
2022-01-01 23:15:56 -0500

    update:

---
+++ src/components/about/About.js
@@ -0,0 +1,25 @@
+import "./about.css";
+
+function About() {
+	return (
+		<div className="master-content">
+			<div className="content">
+				<div className="wrapper">
+					<h1>
+						Hi, my name <br />
+						is <strong>Anthony Vidovic</strong>
+					</h1>
+					<br />
+					<h3>
+						I'm an <strong>independent developer</strong> from{" "}
+						<br />
+						Ontario, Canada
+					</h3>
+				</div>
+			</div>
+			<div className="person"></div>
+		</div>
+	);
+}
+
+export default About;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+	return (
+		<nav className="navbar">
+			<div className="logo">
+				<img src="/logo.png" alt="logo" />
+			</div>
+			<ul>
+				<li>
+					<a href="">About</a>
+				</li>
+				<li>
+					<a
+						target="blank"
+						href="https://www.linkedin.com/in/anthonyvidovic/"
+					>
+						LinkedIn
+					</a>
+				</li>
+				<li>
+					<a href="">Contact</a>
+				</li>
+				<button id="view-work" href="">
+					View Work
+				</button>
+			</ul>
+		</nav>
+	);
+}
+
+export default Navbar;
