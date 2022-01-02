925d8ced37c06b53264c500a3d6331bcb519ec8e
Anthony Vidovic <avidovic@uoguelph.ca>
2022-01-02 17:37:33 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import About from "./components/about/About";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-	// const sbHeight =
-	// 	window.innerHeight * (window.innerHeight / document.body.offsetHeight);
-	// const [scrollPos, setScrollPos] = useState(0.0);
-	// const handleScroll = () => {
-	// 	const position = window.pageYOffset;
-	// 	setScrollPos(position);
-	// };
+	const [scrollPos, setScrollPos] = useState(0.0);
+	const handleScroll = () => {
+		const position = window.pageYOffset;
+		setScrollPos(position);
+	};
 
-	// document.querySelector(".scroll-circle").style.top = scrollPos;
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll, { passive: true });
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-	// useEffect(() => {
-	// 	window.addEventListener("scroll", handleScroll, { passive: true });
-	// 	return () => {
-	// 		window.removeEventListener("scroll", handleScroll);
-	// 	};
-	// }, []);
-	// console.log(scrollPos);
 	return (
 		<div className="page-container">
-			{/* <div
-				className="scroll-circle"
-				style={
-					{
-						top: scrollPos,
-					}
-				}
-			></div> */}
-			<Navbar />
+			<Navbar scrollPos={scrollPos} />
 			<About />
 		</div>
 	);
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 
-function Navbar() {
+function Navbar({ scrollPos }) {
+	// console.log(scrollPos);
+	const [navEffect, setNavEffect] = useState(false);
+
+	const navOnEffect = () => {
+		console.log("onnn");
+		const item1 = document.getElementById("about");
+		const item2 = document.getElementById("media");
+		const item3 = document.getElementById("contact");
+		item1.style.display = "none";
+		item2.style.display = "none";
+		item3.style.display = "none";
+	};
+
+	const navOffEffect = () => {
+		console.log("offf");
+		const item1 = document.getElementById("about");
+		const item2 = document.getElementById("media");
+		const item3 = document.getElementById("contact");
+		item1.style.display = "flex";
+		item2.style.display = "flex";
+		item3.style.display = "flex";
+	};
+
+	if (scrollPos > 300) {
+		if (!navEffect) {
+			navOnEffect();
+			setNavEffect(true);
+		}
+	} else {
+		if (navEffect) {
+			navOffEffect();
+			setNavEffect(false);
+		}
+	}
+
 	return (
 		<nav className="navbar">
 			<div className="logo">
 				<img src="/logo.png" alt="logo" />
 			</div>
 			<ul>
-				<li>
+				<li id="about">
 					<a href="#">About</a>
 				</li>
-				<li>
+				<li id="media">
 					<a
 						target="blank"
 						href="https://www.linkedin.com/in/anthonyvidovic/"
@@ -19,7 +54,7 @@ function Navbar() {
 						LinkedIn
 					</a>
 				</li>
-				<li>
+				<li id="contact">
 					<a href="#">Contact</a>
 				</li>
 				<button id="view-work" href="">
